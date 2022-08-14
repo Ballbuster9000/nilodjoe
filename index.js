c23052191a3c8dfe4eb41a465dc8b1c4a259084e
Ballbuster <93219456+Ballbuster9000@users.noreply.github.com>
2022-08-14 18:47:06 +0300

    read file

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const NILOD = require('nilod')
 const fs = require('fs');
 
 let model = new NILOD();
+
+function printSentence(res) {
+    console.log(`(${res.outputWords.length}) ${res.output}`);
+}
+
+fs.readFile('iy.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  model.train(data)
+});
+ 
